846c696bd79c5b9999ae79d8eecbb0e8df8d9732
Cuong <vunguyencuong1512@gmail.com>
2026-03-20 13:23:01 +0700

    Add API

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,2 +1,16 @@
-from rest_framework import APIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
+
+
+class HelloAPIView(APIView):
+    # Test API View
+    def get(self,request,format=None):
+        
+        # return a list of APIView features
+        an_apiview = [
+            'Uses HTTP methods as function (get,post,patch,put,delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLs',
+        ]
+        return Response({'message':'Hello!','an_apiview':an_apiview})
